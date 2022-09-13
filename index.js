d390b01e9d93ad714e104e39c87c7e1858024d7e
Oksana Stets <oksanastets73@gmail.com>
2022-09-13 21:33:55 +0300

    complete removing notes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import notesList from './data/notes.js';
 // рендер таблиці
 const makeNotesTableRowMarkup = note => {
-  const { name, created, category, content} = note;
+  const { name, created, category, content } = note;
 
-    return `
-  
+  return `  
         <tr>
           <td></td>
           <td>${name}</td>
@@ -17,17 +16,12 @@ const makeNotesTableRowMarkup = note => {
             <button>Archive</button>
             <button type="button" class="delete__button">Delete</button>
           </td>
-          
         </tr>
-  
   `;
-    
 };
 
 const tableEl = document.querySelector('.js-notes-table');
-const notesTableRowsMarkup = notesList
-  .map(makeNotesTableRowMarkup)
-  .join('');
+const notesTableRowsMarkup = notesList.map(makeNotesTableRowMarkup).join('');
 
 tableEl.insertAdjacentHTML('beforeend', notesTableRowsMarkup);
 
@@ -37,51 +31,52 @@ const openFormBtn = document.querySelector('[data-action="open-form"]');
 openFormBtn.addEventListener('click', onOpenForm);
 
 function onOpenForm() {
-    lightbox.classList.add('is-open')
+  lightbox.classList.add('is-open');
 }
 
 const closeFormBtn = document.querySelector('[data-action="close-form"]');
 closeFormBtn.addEventListener('click', onCloseForm);
 
 function onCloseForm() {
-    lightbox.classList.remove('is-open')
+  lightbox.classList.remove('is-open');
 }
 
 // додавання нового запису
 const addNoteForm = document.querySelector('.note-form');
 addNoteForm.addEventListener('submit', onSubmit);
 
-let deleteNoteBtn = document.querySelectorAll('.delete__button');
-
 function onSubmit(event) {
-    event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    const name = formElements.name.value;
-    const created = formElements.created.value;
-    const category = formElements.categ.value;
-    const content = formElements.content.value;
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const name = formElements.name.value;
+  const created = formElements.created.value;
+  const category = formElements.categ.value;
+  const content = formElements.content.value;
 
-    const newNote = {
-        name,
-        created,
-        category,
-        content,
-    }
-    const newNoteRow = makeNotesTableRowMarkup(newNote);
-    const tbodyEl = document.querySelector('tbody')
-    tbodyEl.insertAdjacentHTML('beforeend', newNoteRow);
-    deleteNoteBtn = document.querySelectorAll('.delete__button');
-    console.log(deleteNoteBtn);
-    addNoteForm.reset();
-    onCloseForm();
-    return deleteNoteBtn;
+  const newNote = {
+    name,
+    created,
+    category,
+    content,
+  };
+  const newNoteRow = makeNotesTableRowMarkup(newNote);
+  const tbodyEl = document.querySelector('tbody');
+  tbodyEl.insertAdjacentHTML('beforeend', newNoteRow);
+  addNoteForm.reset();
+  onCloseForm();
 }
+
 // видалення запису
-// const deleteNoteBtn = document.querySelectorAll('.delete__button');
-console.log(deleteNoteBtn);
-for (let i = 0; i < deleteNoteBtn.length; i++) {
-    deleteNoteBtn[i].onclick = function () {
+
+tableEl.addEventListener('click', onDel);
+function onDel(event) {
+  if (event.target.classList.value === 'delete__button') {
+    const deleteNoteBtn = document.querySelectorAll('.delete__button');
+    for (let i = 0; i < deleteNoteBtn.length; i++) {
+      deleteNoteBtn[i].onclick = function () {
         let parent = this.parentNode;
         parent.parentNode.remove();
+      };
     }
+  }
 }
